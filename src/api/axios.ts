79977a68c242353c7da26372ce48915ee5151c08
nikhil.peshwe <nikhilpeshwe@gmail.com>
2026-04-10 02:30:58 -0500

    append api to url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearToken, getToken } from "../auth/tokenStorage";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"}/api`,
   headers: {
     "Content-Type": "application/json",
   },
