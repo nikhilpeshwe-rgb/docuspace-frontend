7ccd22988d62795c372ad3429f5fa2a284cf5a8e
nikhil.peshwe <nikhilpeshwe@gmail.com>
2026-04-14 05:13:02 -0500

    updated vercel and axios settings

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { clearToken, getToken } from "../auth/tokenStorage";
 
+// `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"}/api`, => when using vercel with 
+// render service
+
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"}/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api"}`,
   headers: {
     "Content-Type": "application/json",
   },
